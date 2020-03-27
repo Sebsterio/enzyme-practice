9d71e3af108b436d9dbbe2389007f89ff29a1d85
Sebsterio <sebastian.rosloniec@gmail.com>
2020-03-27 14:41:09 +0100

    add header tests

---
+++ src/component/header/header.spec.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { shallow } from "enzyme";
+import Header from "./header";
+import { findByTestAttr } from "../../utils";
+
+const setUp = (props = {}) => {
+	return shallow(<Header {...props} />);
+};
+
+describe("Header component", () => {
+	let component;
+	beforeEach(() => {
+		component = setUp();
+	});
+
+	it("renders without errors", () => {
+		const wrapper = findByTestAttr(component, "headerComponent");
+		expect(wrapper.length).toBe(1);
+	});
+
+	it("renders the logo", () => {
+		const wrapper = findByTestAttr(component, "logoIMG");
+		expect(wrapper.length).toBe(1);
+	});
+});
